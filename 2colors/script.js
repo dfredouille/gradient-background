196b7947b1f48055aacc26e10ed07c51278ca939
Daniel Fredouille <daniel.fredouille@gmail.com>
2022-02-06 18:45:31 -0500

    Put back proper 2 color version, overriden by mistake

--- 2colors/script.js
+++ 2colors/script.js
@@ -1,8 +1,6 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var color3 = document.querySelector(".color3");
-var color4 = document.querySelector(".color4");
 var body = document.getElementById("gradient");
 var randomButton = document.getElementById("randomButton");
 
@@ -16,10 +14,12 @@ function getRandomColor() {
 }
 
 function setGradient() {
-  
 	body.style.background = 
-  `linear-gradient(to top left, ${color1.value}, rgba(255, 153, 150, 0),  ${color2.value}), `
-  + `linear-gradient(to top right,  ${color3.value}, rgba(255, 153, 150, 0),  ${color4.value}) rgba(255, 153, 150, 1)`
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
 
 	css.textContent = body.style.background + ";";
 }
@@ -28,15 +28,12 @@ function generateAndSetGradient() {
         console.log('IN generateAndSetGradient')
         color1.value = getRandomColor()
         color2.value = getRandomColor()
-        color3.value = getRandomColor()
-        color4.value = getRandomColor()
         setGradient();
 }
 
 color1.addEventListener("input", setGradient);
+
 color2.addEventListener("input", setGradient);
-color3.addEventListener("input", setGradient);
-color4.addEventListener("input", setGradient);
 
 randomButton.addEventListener("click", generateAndSetGradient);
 
